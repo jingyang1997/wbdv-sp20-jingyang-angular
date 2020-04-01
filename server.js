a1510c91f70f06cc578d26604d663b1fd998b3cd
Huskyjjy <jin.jingy@husky.neu.edu>
2020-04-01 00:07:51 -0400

    Fontawesome

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/wbdv-sp20-jannunzi-client-angular'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/myapp/index.html'));});
+    '/dist/wbdv-sp20-jannunzi-client-angular/index.html'));});
 app.listen(process.env.PORT || 8080);
